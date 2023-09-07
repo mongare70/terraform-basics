7ed0042fef0b5f178f4350e3f9c18460f0ce6fc9
Hillary Mongare <hillarymongare70@gmail.com>
2023-09-07 11:24:02 +0300

    first commit

---
+++ main.tf
@@ -0,0 +1,42 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_s3_bucket" "my_s3_bucket" {
+  bucket = "my-s3-bucket-mongare70-001"
+  force_destroy = true
+}
+
+resource "aws_iam_user" "my_iam_user" {
+  name = "my_iam_user_abc_updated"
+  force_destroy = true
+}
+
+resource "aws_s3_bucket_versioning" "versioning_example" {
+  bucket = aws_s3_bucket.my_s3_bucket.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+output "my_s3_bucket_versioning" {
+  value = aws_s3_bucket.my_s3_bucket.versioning[0].enabled
+}
+
+output "my_s3_bucket_complete_details" {
+  value = aws_s3_bucket.my_s3_bucket
+}
+
+output "my_iam_user_complete_details" {
+  value = aws_iam_user.my_iam_user
+}
