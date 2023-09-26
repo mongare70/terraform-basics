33ba7c7c3f5dfa83290ffb5df3f994b1d3f8651a
Hillary Mongare <hillarymongare70@gmail.com>
2023-09-26 16:55:50 +0300

    automated EC2 instance creation & httpd installation

--- 05-ec2 instances/main.tf
+++ 05-ec2 instances/main.tf
@@ -12,6 +12,10 @@ provider "aws" {
   region = "us-east-1"
 }
 
+variable "aws_key_pair" {
+  default = "~/aws/aws_keys/default-ec2.pem"
+}
+
 // HTTP SERVER -> SG
 //SECURITY GROUP(SG) 80 TCP, 22 TCP, CIDR ["0.0.0.0/0"]
 
@@ -55,5 +59,28 @@ resource "aws_security_group" "http_server_sg" {
 
   tags = {
     name = "http_server_sg"
+  }
+}
+
+resource "aws_instance" "http_server" {
+  ami                    = "ami-03a6eaae9938c858c"
+  key_name               = "default ec2"
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.http_server_sg.id]
+  subnet_id              = "subnet-0e1c23f099f55178c"
+
+  connection {
+    type        = "ssh"
+    host        = self.public_ip
+    user        = "ec2-user"
+    private_key = file(var.aws_key_pair)
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "sudo yum install httpd -y",                                                                         // install httpd
+      "sudo service httpd start",                                                                          // start httpd
+      "echo welcome hillary - virtual server is at ${self.public_dns} | sudo tee /var/www/html/index.html" // Copy a file
+    ]
   }
 }
